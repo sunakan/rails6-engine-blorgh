e7ed58b5d1beea9065852bc31ba1d83d9f0c5bbe
sunakan <sunakan03@gmail.com>
2019-05-04 16:27:42 +0900

    [Add] FactoryBotをつかったModelのspecをパスするまで

--- lib/blog_engine/engine.rb
+++ lib/blog_engine/engine.rb
@@ -5,6 +5,7 @@ module BlogEngine
     config.generators do |g|
       g.test_framework :rspec, fixture: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
+      FactoryBot.definition_file_paths << File.expand_path('../../spec/factories', __dir__) if defined?(FactoryBot)
     end
   end
 end

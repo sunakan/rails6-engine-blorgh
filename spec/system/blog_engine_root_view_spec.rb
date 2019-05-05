9ec5bd426a02ce08df75f6eb8ba6217a51ad4549
sunakan <sunakan03@gmail.com>
2019-05-05 18:13:41 +0900

    [Modify] js関連のシステムテストに集中する

--- spec/system/blog_engine_root_view_spec.rb
+++ spec/system/blog_engine_root_view_spec.rb
@@ -5,9 +5,9 @@ describe "blog_engineのrootページ", type: :system do
     visit blog_engine_path
   end
 
-  it "Fuga1という文字列がある" do
-    expect(page).to have_content "Fuga1"
-  end
+  #it "Fuga1という文字列がある" do
+  #  expect(page).to have_content "Fuga1"
+  #end
 
   it "jsによって'Sprocketで追加したh3'という文字列がある", js: true do
     expect(page).to have_content "Sprocketで追加したh3"
